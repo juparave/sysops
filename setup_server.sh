b3f454f343ab4dc062e4b2583ee642824134496c
Juan Pablo TankerMini <juparave@deuxbits.com>
2021-12-31 12:41:38 -0600

    ..

--- setup_server.sh
+++ setup_server.sh
@@ -7,7 +7,7 @@ ENDCOLOR="\e[0m"
 # Install a new server with common apps
 # Assuming this is an Ubuntu distro
 
-echo "${GREEN}Updating instance${ENDCOLOR}"
+echo -e "${GREEN}Updating instance${ENDCOLOR}"
 apt-get update
 apt-get install aptitude
 aptitude -y safe-upgrade
