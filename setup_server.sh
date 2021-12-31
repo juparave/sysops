3de9d8bc59c56c39f4c117538e6919c225ad10db
Juan Pablo TankerMini <juparave@deuxbits.com>
2021-12-31 12:56:11 -0600

    ..

--- setup_server.sh
+++ setup_server.sh
@@ -7,7 +7,7 @@ ENDCOLR=$(tput setaf 7)
 # Install a new server with common apps
 # Assuming this is an Ubuntu distro
 
-echo -e "${BLUE}Updating instance${ENDCOLR}"
+echo "${BLUE}Updating instance${ENDCOLR}"
 apt-get update
 apt-get install aptitude
 aptitude -y safe-upgrade
